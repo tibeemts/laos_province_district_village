34f5873230111694ff038be2ec7894a959008e32
Tibee Bouasavanh <72777657+tibeemts@users.noreply.github.com>
2023-11-04 20:26:21 +0700

    Update provinces.sql

--- provinces.sql
+++ provinces.sql
@@ -1,34 +1,16 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : dbmts
- Source Server Type    : PostgreSQL
- Source Server Version : 120015 (120015)
- Source Host           : dbmts.mtsapi.com:5432
- Source Catalog        : prfmis
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 120015 (120015)
- File Encoding         : 65001
-
- Date: 18/07/2023 14:44:20
-*/
-
-
 -- ----------------------------
 -- Table structure for provinces
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."provinces";
 CREATE TABLE "public"."provinces" (
-  "prov_id" varchar(2) COLLATE "pg_catalog"."default" NOT NULL,
-  "prov_sym" varchar(10) COLLATE "pg_catalog"."default",
-  "prov_lo" varchar(50) COLLATE "pg_catalog"."default",
-  "prov_en" varchar(50) COLLATE "pg_catalog"."default"
+  "prov_id" int4 NOT NULL,
+  "prov_en" varchar(255) COLLATE "pg_catalog"."default",
+  "prov_lo" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
+ALTER TABLE "public"."provinces" OWNER TO "root";
 
 -- ----------------------------
 -- Primary Key structure for table provinces
 -- ----------------------------
-ALTER TABLE "public"."provinces" ADD CONSTRAINT "provinces_pkey" PRIMARY KEY ("prov_id");
+ALTER TABLE "public"."provinces" ADD CONSTRAINT "province_pkey" PRIMARY KEY ("prov_id");
