c8c9931c517e5d083a501f30e5c414c654004789
Tibee Bouasavanh <72777657+tibeemts@users.noreply.github.com>
2023-11-04 20:29:20 +0700

    Update districts.sql

--- districts.sql
+++ districts.sql
@@ -1,32 +1,15 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : dbmts
- Source Server Type    : PostgreSQL
- Source Server Version : 120015 (120015)
- Source Host           : dbmts.mtsapi.com:5432
- Source Catalog        : prfmis
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 120015 (120015)
- File Encoding         : 65001
-
- Date: 18/07/2023 14:45:43
-*/
-
-
 -- ----------------------------
 -- Table structure for districts
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."districts";
 CREATE TABLE "public"."districts" (
-  "prov_id" varchar(2) COLLATE "pg_catalog"."default",
-  "dist_id" varchar(10) COLLATE "pg_catalog"."default" NOT NULL,
-  "dist_lo" varchar(50) COLLATE "pg_catalog"."default",
-  "dist_en" varchar(50) COLLATE "pg_catalog"."default"
+  "dist_id" int4 NOT NULL,
+  "dist_lo" varchar(255) COLLATE "pg_catalog"."default",
+  "dist_en" varchar(255) COLLATE "pg_catalog"."default",
+  "prov_id" int4
 )
 ;
+ALTER TABLE "public"."districts" OWNER TO "root";
 
 -- ----------------------------
 -- Primary Key structure for table districts
@@ -36,4 +19,4 @@ ALTER TABLE "public"."districts" ADD CONSTRAINT "districts_pkey" PRIMARY KEY ("d
 -- ----------------------------
 -- Foreign Keys structure for table districts
 -- ----------------------------
-ALTER TABLE "public"."districts" ADD CONSTRAINT "districts_prov_id_fkey" FOREIGN KEY ("prov_id") REFERENCES "public"."provinces" ("prov_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."districts" ADD CONSTRAINT "prov_id" FOREIGN KEY ("prov_id") REFERENCES "public"."provinces" ("prov_id") ON DELETE RESTRICT ON UPDATE CASCADE;
